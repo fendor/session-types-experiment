3edd4e19d453b6844ee7fed9d372fe53ba492d99
Fendor <power.walross@gmail.com>
2022-10-11 10:05:51 +0200

    Format effects-as-session code

--- effects-as-sessions/Control/Effect.hs
+++ effects-as-sessions/Control/Effect.hs
@@ -6,26 +6,27 @@ import Prelude hiding (Monad(..))
 import GHC.Prim
 import Data.Kind (Constraint)
 
-{-| Specifies "parametric effect monads" which are essentially monads but
-     annotated by a type-level monoid formed by 'Plus' and 'Unit' -}
+{- | Specifies "parametric effect monads" which are essentially monads but
+     annotated by a type-level monoid formed by 'Plus' and 'Unit'
+-}
 class Effect (m :: k -> * -> *) where
-
-   {-| Effect of a trivially effectful computation |-}
+   -- | Effect of a trivially effectful computation |
    type Unit m :: k
-   {-| Cominbing effects of two subcomputations |-}
+
+   -- | Cominbing effects of two subcomputations |
    type Plus m (f :: k) (g :: k) :: k
 
-   {-| 'Inv' provides a way to give instances of 'Effect' their own constraints for '>>=' -}
+   -- | 'Inv' provides a way to give instances of 'Effect' their own constraints for '>>='
    type Inv m (f :: k) (g :: k) :: Constraint
+
    type Inv m f g = ()
 
-   {-| Effect-parameterised version of 'return'. Annotated with the 'Unit m' effect,
-    denoting pure compuation -}
+   -- | Effect-parameterised version of 'return'. Annotated with the 'Unit m' effect,
+   --    denoting pure compuation
    return :: a -> m (Unit m) a
 
-   {-| Effect-parameterise version of '>>=' (bind). Combines
-    two effect annotations 'f' and 'g' on its parameter computations into 'Plus' -}
-
+   -- | Effect-parameterise version of '>>=' (bind). Combines
+   --    two effect annotations 'f' and 'g' on its parameter computations into 'Plus'
    (>>=) :: (Inv m f g) => m f a -> (a -> m g b) -> m (Plus m f g) b
 
    (>>) :: (Inv m f g) => m f a -> m g b -> m (Plus m f g) b
@@ -33,7 +34,6 @@ class Effect (m :: k -> * -> *) where
 
 fail = undefined
 
-{-| Specifies subeffecting behaviour -}
+-- | Specifies subeffecting behaviour
 class Subeffect (m :: k -> * -> *) f g where
-    sub :: m f a -> m g a
-
+   sub :: m f a -> m g a
--- effects-as-sessions/Control/Effect/Sessions/Operations.hs
+++ effects-as-sessions/Control/Effect/Sessions/Operations.hs
@@ -20,19 +20,19 @@ import Control.Concurrent.STM.TMVar
 
 import Control.Effect (Subeffect(..))
 
-{-| A process can be run if it is /closed/ (i.e., empty channel environment) -}
+-- | A process can be run if it is /closed/ (i.e., empty channel environment)
 run :: Process '[] a -> IO a
 run = getProcess
 
-{-| Lift IO computations to a process -}
+-- | Lift IO computations to a process
 liftIO :: IO a -> Process '[] a
 liftIO = Process
 
-{-| Print to stdout in a process -}
+-- | Print to stdout in a process
 print :: Show a => a -> Process '[] ()
 print = liftIO . (Prelude.print)
 
-{-| putStrLn in a process -}
+-- | putStrLn in a process
 putStrLn = liftIO . Prelude.putStrLn
 
 -- The simplest operations, send and receive of primitive values,
@@ -40,11 +40,11 @@ putStrLn = liftIO . Prelude.putStrLn
 -- indexed by the session environment '[c :-> S]' where 'S' is either a
 -- send or receive action (terminated by 'End').
 
-{-| Send a primitive-typed value -}
+-- | Send a primitive-typed value
 send :: Chan c -> t -> Process '[c :-> t :! End] ()
 send (MkChan c) t = Process $ C.writeChan (unsafeCoerce c) t
 
-{-| Receive a primitive-typed value -}
+-- | Receive a primitive-typed value
 recv :: Chan c -> Process '[c :-> t :? End] t
 recv (MkChan c) = Process $ C.readChan (unsafeCoerce c) -- >>= (return . unWrap)
 
@@ -53,53 +53,65 @@ recv (MkChan c) = Process $ C.readChan (unsafeCoerce c) -- >>= (return . unWrap)
 -- 'Ch c' and 'Op C' to a session with behaviour 's', and creates
 -- a session where any mention to 'Ch c' or 'Op c' is removed:
 
-{-| Create a new channel and pass its two endpoints to the supplied continuation
-     (the first parameter). This channel is thus only in scope for this continuation -}
-new :: (Duality env c)
-          =>  ((Chan (Ch c), Chan (Op c)) -> Process env t)
-          ->  Process (env :\ (Op c) :\ (Ch c)) t
+{- | Create a new channel and pass its two endpoints to the supplied continuation
+     (the first parameter). This channel is thus only in scope for this continuation
+-}
+new ::
+  (Duality env c) =>
+  ((Chan (Ch c), Chan (Op c)) -> Process env t) ->
+  Process (env :\ (Op c) :\ (Ch c)) t
 new f = Process $ C.newChan >>= (\c -> getProcess $ f (MkChan c, MkChan c))
 
-{-| Parallel compose two processes, if they contain balanced sessions -}
-par :: (BalancedPar env env') =>
-        Process env () -> Process env' () -> Process (DisjointUnion env env') ()
-par (Process x) (Process y) =  Process $ do res <- newEmptyTMVarIO
-                                            res' <- newEmptyTMVarIO
-                                            Conc.forkIO $ (x >>= (atomically . (putTMVar res)))
-                                            Conc.forkIO $ (y >>= (atomically . (putTMVar res')))
-                                            () <- atomically $ do { takeTMVar res }
-                                            () <- atomically $ do { takeTMVar res' }
-                                            return ()
+-- | Parallel compose two processes, if they contain balanced sessions
+par ::
+  (BalancedPar env env') =>
+  Process env () ->
+  Process env' () ->
+  Process (DisjointUnion env env') ()
+par (Process x) (Process y) = Process $ do
+  res <- newEmptyTMVarIO
+  res' <- newEmptyTMVarIO
+  Conc.forkIO $ (x >>= (atomically . (putTMVar res)))
+  Conc.forkIO $ (y >>= (atomically . (putTMVar res')))
+  () <- atomically $ do takeTMVar res
+  () <- atomically $ do takeTMVar res'
+  return ()
 
-{-| Turn all session types into 'balancing checked' session types |-}
+-- | Turn all session types into 'balancing checked' session types |
 type family AllBal (env :: [Map Name Session]) :: [Map Name Session] where
-            AllBal '[] = '[]
-            AllBal ((c :-> s) ': env) = (c :-> Bal s) ': (AllBal env)
+  AllBal '[] = '[]
+  AllBal ((c :-> s) ': env) = (c :-> Bal s) ': (AllBal env)
 
-{-| Output a channel (dual to a replicated input) -}
+-- | Output a channel (dual to a replicated input)
 rsend :: Chan c -> Chan d -> Process '[c :-> (Delg s) :*! End, d :-> Bal s] ()
 rsend (MkChan c) t = Process $ C.writeChan (unsafeCoerce c) t
 
 -- Channels can then be sent and received with the 'chSend' and 'chRecv' primitives:
 
-{-| Send a channel 'd' over channel 'c' -}
+-- | Send a channel 'd' over channel 'c'
 chSend :: Chan c -> Chan d -> Process '[c :-> (Delg s) :! End, d :-> Bal s] ()
 chSend (MkChan c) t = Process $ C.writeChan (unsafeCoerce c) t
 
-{-| Receive a channel over a channel 'c', bind to the name 'd' -}
+-- | Receive a channel over a channel 'c', bind to the name 'd'
 
 --  chRecv :: Chan c -> (Chan d -> Process env a) ->
 --             Process (UnionSeq '[c :-> (Delg (env :@ d)) :? (env :@ c)] (env :\ d)) a
 --  chRecv (MkChan c) f = Process $ C.readChan (unsafeCoerce c) >>=
 --                                      (getProcess . f . unsafeCoerce)
 
-chRecv :: Chan c -> Process '[c :-> (Delg (env :@ d)) :? End]
-                        ((Chan d -> Process env a) -> Process (env :\ d) a)
-chRecv (MkChan c) = Process $ return $
-                               \f -> let y = (C.readChan (unsafeCoerce c) >>=
-                                                     (getProcess . f . unsafeCoerce))
-                                     in Process $ y
-
+chRecv ::
+  Chan c ->
+  Process
+    '[c :-> (Delg (env :@ d)) :? End]
+    ((Chan d -> Process env a) -> Process (env :\ d) a)
+chRecv (MkChan c) = Process $
+  return $
+    \f ->
+      let y =
+            ( C.readChan (unsafeCoerce c)
+                >>= (getProcess . f . unsafeCoerce)
+            )
+       in Process $ y
 
 -- Given a channel 'c', and a computation which binds channel 'd' to produces behaviour
 -- 'c', then this is provided by receiving 'd' over 'c'. Thus the resulting computation
@@ -111,37 +123,45 @@ chRecv (MkChan c) = Process $ return $
 -- Subeffecting instances for the least-upper bound :+ operator and for extending
 -- an environment with a closed session channel, i.e. with c :-> End.
 
-instance Subeffect Process env env' =>
-         Subeffect Process ((c :-> s) ': env) ((c :-> s :+ t) ': env') where
-    sub (Process p) = Process p
+instance
+  Subeffect Process env env' =>
+  Subeffect Process ((c :-> s) ': env) ((c :-> s :+ t) ': env')
+  where
+  sub (Process p) = Process p
 
-instance Subeffect Process env env' =>
-         Subeffect Process ((c :-> t) ': env) ((c :-> s :+ t) ': env') where
-    sub (Process p) = Process p
+instance
+  Subeffect Process env env' =>
+  Subeffect Process ((c :-> t) ': env) ((c :-> s :+ t) ': env')
+  where
+  sub (Process p) = Process p
 
 instance Subeffect Process env env where
-    sub = id
+  sub = id
 
-instance Subeffect Process env env' =>
-         Subeffect Process ((c :-> s) ': env) ((c :-> s) ': env') where
-    sub (Process p) = Process p
+instance
+  Subeffect Process env env' =>
+  Subeffect Process ((c :-> s) ': env) ((c :-> s) ': env')
+  where
+  sub (Process p) = Process p
 
-instance Subeffect Process env env' =>
-         Subeffect Process env ((c :-> End) ': env') where
-    sub (Process p) = Process p
+instance
+  Subeffect Process env env' =>
+  Subeffect Process env ((c :-> End) ': env')
+  where
+  sub (Process p) = Process p
 
 -- Explicit subeffecting operations for subtyping on the left of a conditional
 -- and subtyping on the right of a conditional
 
-{-| Explicit subeffecting introduction of ':+' with the current session behaviour on the left -}
+-- | Explicit subeffecting introduction of ':+' with the current session behaviour on the left
 subL :: Process '[c :-> s] a -> Process '[c :-> s :+ t] a
 subL = sub
 
-{-| Explicit subeffecting introduction of ':+' with the current session behaviour on the right -}
+-- | Explicit subeffecting introduction of ':+' with the current session behaviour on the right
 subR :: Process '[c :-> t] a -> Process '[c :-> s :+ t] a
 subR = sub
 
-{-| Explicit subeffecting introduction of a terminated session for channel 'c' -}
+-- | Explicit subeffecting introduction of a terminated session for channel 'c'
 subEnd :: Chan c -> Process '[] a -> Process '[c :-> End] a
 subEnd _ = sub
 
@@ -151,13 +171,16 @@ subEnd _ = sub
 caseUnion :: Chan c -> Process env a -> Process ((c :-> s) ': env) a
 caseUnion _ (Process p) = Process p
 
-
-{-| Recursion combinator for recursive functions which have an affine fixed-point
+{- | Recursion combinator for recursive functions which have an affine fixed-point
     equation on their effects.
       For example, if 'h ~ (Seq h a) :+ b' then 'ToFix h = Fix a b,'
-   -}
-affineFix :: ((a -> Process '[c :-> Star] b)
-          -> (a -> Process '[c :-> h] b))
-          -> a -> Process '[c :-> ToFix h] b
-affineFix f x = let (Process p) = f (\x' -> let (Process y) = affineFix f x' in Process y) x
-                 in Process p
+-}
+affineFix ::
+  ( (a -> Process '[c :-> Star] b) ->
+    (a -> Process '[c :-> h] b)
+  ) ->
+  a ->
+  Process '[c :-> ToFix h] b
+affineFix f x =
+  let (Process p) = f (\x' -> let (Process y) = affineFix f x' in Process y) x
+   in Process p
