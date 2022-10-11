9082d430b923928edea4ede2e6098fce0337879e
Fendor <power.walross@gmail.com>
2022-10-11 11:09:22 +0200

    Vendor type-level-sets for ghc 9.4

---
+++ effects-as-sessions/vendored/type-level-sets-0.8.9.0/example.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE DataKinds, TypeOperators, TypeFamilies, MultiParamTypeClasses #-}
+
+import GHC.TypeLits
+import Data.Type.Map
+
+-- Specify that key-value pairs on Ints combine to an Int
+type instance Combine Int Int = Int
+-- Specify that Int values for matching keys should be added
+instance Combinable Int Int where
+    combine x y = x + y
+
+foo :: Map '["x" :-> Int, "z" :-> Bool, "w" :-> Int]
+foo = Ext (Var :: (Var "x")) 2
+    $ Ext (Var :: (Var "z")) True
+    $ Ext (Var :: (Var "w")) 5
+    $ Empty
+
+foo' :: Map (AsMap '["z" :-> Bool, "x" :-> Int, "w" :-> Int])
+foo' = asMap foo
+
+bar :: Map '["y" :-> Int, "w" :-> Int]
+bar = Ext (Var :: (Var "y")) 3 $
+       Ext (Var :: (Var "w")) 1 $
+         Empty
+
+-- GHC can easily infer this type, so an explicit signature not necessary
+-- foobar :: Map '["w" :-> Int, "x" :-> Int, "y" :-> Integer, "z" :-> Int]
+foobar = foo `union` bar
+
+foobarToFoo :: Map '["w" :-> Int, "x" :-> Int, "z" :-> Bool]
+foobarToFoo = submap foobar
---
+++ effects-as-sessions/vendored/type-level-sets-0.8.9.0/example2.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DataKinds, TypeOperators, TypeFamilies, GADTs, StandaloneDeriving #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+import GHC.TypeLits
+import Data.Type.Set
+
+type instance Cmp (Natural n) (Natural m) = CmpNat n m
+
+data Natural (a :: Nat) where
+    Z :: Natural 0
+    S :: Natural n -> Natural (n + 1)
+
+deriving instance Show (Natural n)
+
+-- foo :: Set '[Natural 0, Natural 1, Natural 3]
+foo = asSet $ Ext (S Z) (Ext (S (S (S Z))) (Ext Z Empty))
+
+-- bar :: Set '[Natural 1, Natural 2]
+bar = asSet $ Ext (S (S Z)) (Ext (S Z) (Ext (S Z) Empty))
+
+-- foobar :: Set '[Natural 0, Natural 1, Natural 2, Natural 3]
+foobar = foo `union` bar
+
+nonMemberTest :: NonMember (Natural 0) as => Set as -> ()
+nonMemberTest set = ()
+
+-- nonMemberTest meep  is well typed
+meep = asSet $ Ext (S Z) (Ext (S (S Z)) Empty)
+-- nonMemberTest morp is ill typed
+morp = asSet $ Ext (S Z) (Ext (S (S Z)) (Ext Z Empty))
